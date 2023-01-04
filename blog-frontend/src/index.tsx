0108c3f781cebe3c6294791878db86fb23d64693
Ramon Zuniga <ramon@fullstacklabs.co>
2023-01-04 17:03:50 -0600

    Modernized

--- blog-frontend/src/index.tsx
+++ blog-frontend/src/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Auth0Provider } from './contexts/auth0-context';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-	<Auth0Provider>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</Auth0Provider>,
-	document.getElementById('root')
+  <Auth0Provider
+    domain="dev-o4oau070lcbfht7w.us.auth0.com"
+    clientId="NXCmRS59dXIGMXqJ59jcHr8JUCM6yXNw"
+    redirectUri={window.location.origin}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
